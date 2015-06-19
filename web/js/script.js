5b49c1a4256269d2e9589756c2102aeaaf340f5b
annakriener <anna.kriener@gmail.com>
2015-06-19 19:36:32 +0200

    Suchformular, Rezept-Ergebnislisten sind jetzt responsive

--- web/js/script.js
+++ web/js/script.js
@@ -12347,17 +12347,17 @@ function renderIngredients(annoted) {
  * Created by Anna Kriener on 15.06.2015.
  */
 $(document).ready(function (){
-   var searchRefineButton = $('button#cb-search-refine-button');
+   var searchRefineButton = $('button#cb-search-refine-button-down');
     var searchRefineDiv = $('div#cb-search-refine');
     var searchForm = $('form#cb-search-form');
 
     searchRefineDiv.on('show.bs.collapse', function(event) {
         searchForm.fadeOut();
-        searchRefineButton.children('span').attr('class', 'glyphicon glyphicon-collapse-up');
+        searchRefineButton.fadeOut();
 
     }).on('hide.bs.collapse', function(event) {
         searchForm.fadeIn();
-        searchRefineButton.children('span').attr('class', 'glyphicon glyphicon-collapse-down');
+        searchRefineButton.fadeIn();
     });
 });
 $(document).ready(function () {
